dcd10f4b7e49af27786470000d7ffd36441fc114
Eryk Tavares <EricTavares35@gmail.com>
2026-09-18 22:56:09 -0300

    feat(projetos): refactor Projetos component for improved readability

--- src/pages/projetos/Projetos.jsx
+++ src/pages/projetos/Projetos.jsx
@@ -5,26 +5,28 @@ import PageTitle from '../../components/PageTitle';
 import ProjectCard from '../../components/ProjectCard';
 import DefaultLayout from '../../layout/defaultLayout';
 
-const projectlist = mock;
+const projectListData = mock;
 
-const Projetos = () => (
-	<DefaultLayout>
-		<ProjectStyle>
-			<PageTitle>Projetos</PageTitle>
-			<ProjectGridStyle>
-				{projectlist.map(({ name, description, gif, link, category }, index) => (
-					<ProjectCard
-						key={index}
-						title={name}
-						description={description}
-						gif={gif}
-						link={link}
-						category={category}
-					/>
-				))}
-			</ProjectGridStyle>
-		</ProjectStyle>
-	</DefaultLayout>
-);
+const Projetos = () => {
+	const projectList = projectListData.map(({ name, description, gif, link, category }, index) => (
+		<ProjectCard
+			key={index}
+			title={name}
+			description={description}
+			gif={gif}
+			link={link}
+			category={category}
+		/>
+	));
+
+	return (
+		<DefaultLayout>
+			<ProjectStyle>
+				<PageTitle>Projetos</PageTitle>
+				<ProjectGridStyle>{projectList}</ProjectGridStyle>
+			</ProjectStyle>
+		</DefaultLayout>
+	);
+};
 
 export default Projetos;
